e719e7c875e4f9acafea32c6feb4627273e6ac2f
M0rica <65033374+M0rica@users.noreply.github.com>
2021-02-04 18:43:06 +0100

    changes in video resizing

--- ready-to-use/Display/resize.py
+++ ready-to-use/Display/resize.py
@@ -2,7 +2,9 @@ import os
 from PIL import Image
 import cv2
 import argparse
+from cv2 import data
 import numpy as np
+import json
 
 
 def resize_image(img: Image, resolution: tuple):
@@ -22,18 +24,40 @@ def resize_image_from_disk(path: str, resolution: tuple):
 def resize_video(path: str, resolution: tuple):
     vidcap = cv2.VideoCapture(path)
     success, img = vidcap.read()
+    data_list = []
+    frames = 0
 
-    output_video = cv2.VideoWriter(f'plugins/Display/resized/{path.split("/")[-1]}', 0, 20, resolution)
+    #output_video = cv2.VideoWriter(f'plugins/Display/resized/{path.split("/")[-1]}', 0, 20, resolution)
     while success:
         img = resize_image(Image.fromarray(img), resolution)
         img = np.asarray(img)
+        #output_video.write(img)
+        img_list = img.tolist()
+        frame = []
+        #print(len(img_list), len(img_list[0]))
+        for i in range(0, len(img_list)):
+            line = []
+            for j in range(0, len(img_list[0])):
+                current = img_list[i][j]
+                line.append(current[0]*current[1]*current[2])
+            frame.append(line)
+        frames += 1
 
-        output_video.write(img)
+        data_list.append(frame)
+
 
         success, img = vidcap.read()
     
-    cv2.destroyAllWindows()
-    output_video.release()
+    #cv2.destroyAllWindows()
+    #output_video.release()
+    #print(data_list[0])
+
+    save_data = {
+        'frames': frames,
+        'data': data_list
+    }
+    with open('plugins/Display/resized/video.json', 'w') as f:
+        json.dump(save_data, f, indent=4)
 
 parser = argparse.ArgumentParser(description='Argparser for resize')
 
@@ -49,8 +73,9 @@ print(file, resolution)
 if not os.path.isdir('plugins/Display/resized'):
     os.mkdir('plugins/Display/resized')
 
-if len(os.listdir('plugins/Display/resized')) == 1:
-    os.remove(f'plugins/Display/resized/{os.listdir("plugins/Display/resized")[0]}')
+if len(os.listdir('plugins/Display/resized')) > 1:
+    for f in os.listdir("plugins/Display/resized"):
+        os.remove(f'plugins/Display/resized/{f}')
 
 if file.split('/')[2] == 'image':
     resize_image_from_disk(file, tuple(resolution))
